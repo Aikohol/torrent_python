3a985f8cf80a5aa322dfc4740f340630a3ad525f
Tio Gobin <tio.gobin@epitech.eu>
2019-05-07 11:47:16 +0200

    Little correction on branch problem

--- gui.py
+++ gui.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import tkinter.filedialog
-from tkinter import ttk
 from utils.torrent import torrent_start
 import logging
 
@@ -12,8 +11,6 @@ class Application(tk.Frame):
         self.pack()
         self.create_downlaod_button()
         self.create_download_input()
-        self.create_input_download()
-        self.create_progress_bar()
 
     def create_downlaod_button(self):
         self.download_button = tk.Button(self)
@@ -31,25 +28,15 @@ class Application(tk.Frame):
         self.download_button["command"] = self.askopenfile
         self.download_button.pack(side="top")
 
-    def create_progress_bar(self):
-        self.progress_bar = ttk.Progressbar(self, orient='horizontal', length=400, mode='determinate')
-        self.progress_bar.pack()
-        self.progress_bar['maximum'] = 100
-        self.progress_bar['value'] = 30
-
-    def create_input_download(self):
-        self.download_input = tk.Entry(self, textvariable = '')
-
     def download_button_action(self):
         torrent_start(self.filepath)
 
     def askopenfile(self):
-        file = tkinter.filedialog.askopenfile(initialdir = "./", title='Select file')
-        pathfile git = file.name
+        file = tkinter.filedialog.askopenfile(initialdir = "./", title='Select file', filetypes = (("torrent files",".torrent"),("all files",".*")))
+        logging.warning(file)
+        self.filepath = file.name
 
 
 root = tk.Tk()
-root.filename = filedialog.askopenfilename(initialdir = "/", title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-print (root.filename)
 app = Application(master=root)
 app.mainloop()
