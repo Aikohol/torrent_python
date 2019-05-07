8bf39e81e223da22dcc80c11c7d69883584866bb
Sovannara Hem <sovannara.hem@epitech.eu>
2019-05-07 17:08:58 +0200

    checkout on new branch

--- gui.py
+++ gui.py
@@ -1,53 +1,78 @@
 import tkinter as tk
 import tkinter.filedialog
 from tkinter import ttk
-from utils.torrent import torrent_start
+from utils.torrent import Torrent
+
 import logging
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.filepath = ""
+        self.directorypath = ""
         self.master = master
+        self.torrents = {}
         self.pack()
         self.create_downlaod_button()
         self.create_download_input()
-        self.create_input_download()
-        self.create_progress_bar()
+        if self.torrents:
+            self.create_progress_bar()
+        # self.create_save_button()
+
 
     def create_downlaod_button(self):
         self.download_button = tk.Button(self)
         self.download_button["text"] = "Start Download"
         self.download_button["command"] = self.download_button_action
-        self.download_button.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.download_button.pack()
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack()
 
     def create_download_input(self):
         self.download_button = tk.Button(self)
         self.download_button["text"] = "Browse"
         self.download_button["command"] = self.askopenfile
-        self.download_button.pack(side="top")
+        self.download_button.pack()
 
-    def create_progress_bar(self):
-        self.progress_bar = ttk.Progressbar(self, orient='horizontal', length=400, mode='determinate')
-        self.progress_bar.pack()
-        self.progress_bar['maximum'] = 100
-        self.progress_bar['value'] = 30
-
-    def create_input_download(self):
-        self.download_input = tk.Entry(self, textvariable = '')
+    def create_save_button(self):
+        self.save_button = tk.Button(self)
+        self.save_button["text"] = "Choose directory"
+        self.save_button["command"] = self.askdirectory
+        self.save_button.pack()
 
     def download_button_action(self):
-        torrent_start(self.filepath)
+        torrent = Torrent()
+        torrent.load_torrent(self.filepath, self.directorypath)
+        torrent.execute_torrent()
+        self.torrents.update({torrent.name: torrent})
 
     def askopenfile(self):
         file = tkinter.filedialog.askopenfile(initialdir = "./", title='Select file')
-        pathfile = file.name
+        self.filepath = file.name
+
+    def askdirectory(self):
+        self.directorypath = tkinter.filedialog.askdirectory()
+
+    def create_progress_bar(self):
+        style = ttk.Style()
+        style.configure("orange.Horizontal.TProgressbar", foreground='#ff752d', background='#ff752d')
+        self.progress_bar = ttk.Progressbar(self, style='orange.Horizontal.TProgressbar', orient='horizontal',
+                                            length=400, mode='determinate')
+        self.progress_bar['maximum'] = 100
+        logging.warning(self.torrents)
+        self.progress_bar['value'] = self.torrents
+        self.progress_bar.pack()
 
 
 root = tk.Tk()
+root.config(background='black')
 app = Application(master=root)
+app.config(background='#1f232e', width='500')
+app.pack(side='left', fill='y')
+app.pack_propagate(0)
+# download = Download()
+# download.config(background='#2c2f36', width='900')
+# download.pack(side='left', fill='y')
+# download.pack_propagate(0)
+# download.mainloop()
 app.mainloop()
--- utils/torrent.py
+++ utils/torrent.py
@@ -1,30 +1,44 @@
 import libtorrent as lt
 import time
 import sys
+from threading import Thread
+import logging
 
 
-def torrent_start(filepath):
-    ses = lt.session({'listen_interfaces': '0.0.0.0:8080'})
+class Torrent(object):
+    def __init__(self):
+        self.is_downloading = True
+        self.ses = {}
+        self.h = {}
+        self.name = ""
+        self.progress = 0
+    def load_torrent(self, filepath, directory):
+        self.ses = lt.session({'listen_interfaces': '0.0.0.0:8080'})
 
-    info = lt.torrent_info(filepath)
-    h = ses.add_torrent({'ti': info, 'save_path': '.'})
-    s = h.status()
-    print('starting', s.name)
+        info = lt.torrent_info(filepath)
+        self.h = self.ses.add_torrent({'ti': info, 'save_path': directory + '/'})
+        self.name = filepath
 
-    while (not s.is_seeding):
-        s = h.status()
 
-        print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
-            s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
-            s.num_peers, s.state), end=' ')
+    def execute_torrent(self):
+        s = self.h.status()
+        print('starting', s.name)
+        logging.warning(s)
+        while (not s.is_seeding and self.is_downloading):
+            s = self.h.status()
 
-        alerts = ses.pop_alerts()
-        for a in alerts:
-            if a.category() & lt.alert.category_t.error_notification:
-                print(a)
+            print('\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
+                s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
+                s.num_peers, s.state), end=' ')
+            self.progress = s.progress
+            alerts = self.ses.pop_alerts()
+            for a in alerts:
+                if a.category() & lt.alert.category_t.error_notification:
+                    print(a)
 
-        sys.stdout.flush()
+            sys.stdout.flush()
 
-        time.sleep(1)
+            time.sleep(1)
 
-    print(h.name(), 'complete')
+        print(self.h.name(), 'complete')
+
